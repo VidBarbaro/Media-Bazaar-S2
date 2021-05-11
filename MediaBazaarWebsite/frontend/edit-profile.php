7a9cc3a6bc2bd73291588513d5e7eede1e46b582
Niţu,Ana A.M <a.nitu@student.fontys.nl>
2021-05-11 16:57:49 +0000

    Add Website Frontend with css and images

---
+++ MediaBazaarWebsite/frontend/edit-profile.php
@@ -0,0 +1,72 @@
+<?php
+  include ('header.php');
+?>
+
+<html>
+<head>
+    <title>Profile - Page</title>
+    <link  rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+
+<?php
+    require_once('../includes/db-inc.php');
+    require_once('../includes/Administration.php');
+
+    $email = $_SESSION['email'];
+    $emailExists1 = $adm->emailExistsEmployee($conn, $email);
+    $emailExists2 = $adm->emailExistsManager($conn, $email);
+
+    if($emailExists1 == true && $emailExists2 == false) //it's an employee
+    {
+      $phoneNr = $emailExists1["phonenumber"];
+      $address = $emailExists1["address"];
+    }
+
+    if($emailExists1 == false && $emailExists2 == true) //it's a manager
+    {
+      $phoneNr = $emailExists2["phonenumber"];
+      $address = $emailExists2["address"];
+    }
+?>
+
+
+<body class="profile-page">
+    <div class="container">
+        <main>
+            <div class="card-container">
+
+                <div class="upper-container">
+                    <div class="image-container">
+                        <img src="../images/profile-page.jpeg">
+                    </div>
+                </div>
+
+                <div class="lower-container">
+                    <form action="../includes/edit-profile-user.inc.php" method="post">
+                        <div>
+                            <br>
+                            <h4>Phone Number:</h3>
+                            <br>
+                            <input type="text" name="currentPhoneNumber" value="<?php echo $phoneNr;?>">
+                            <br><br>
+                            <h4>Address:</h3>
+                            <br>
+                            <input type="text" name="currentAddress" value="<?php echo $address;?>">
+                            <br><br>
+                            <input type="submit" name="submit" value="Edit Profile" class="btn"></a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </main>
+
+        <?php
+            include ('sidebar.php');
+        ?>
+
+    </div>
+</body>
+</html>
